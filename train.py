3e8c67252a9570ebeee0cb9dcb24e878d8fc7ecb
bassamtiano <bassamtiano@gmail.com>
2022-10-05 21:19:08 +0700

    Fixing Statistical Function

--- train.py
+++ train.py
@@ -102,40 +102,40 @@ def preprocess():
     
     datasets = pd.concat([data_beras, data_bbm["harga_bbm"]], axis=1)
 
-    print(dataset)
-    # print(datasets)
-    # datasets["harga_bbm"] = datasets["harga_bbm"].apply(lambda x: f"{repair_digit_harga_bbm(x)}")
-    # datasets["harga_bbm"] = pd.to_numeric(datasets["harga_bbm"])
+    
+    print(datasets)
+    datasets["harga_bbm"] = datasets["harga_bbm"].apply(lambda x: f"{repair_digit_harga_bbm(x)}")
+    datasets["harga_bbm"] = pd.to_numeric(datasets["harga_bbm"])
 
-    # data_bbm = datasets[["tahun", "bulan", "harga_bbm"]]
-    # data_beras = datasets[["tahun", "bulan", "harga_beras"]]
+    data_bbm = datasets[["tahun", "bulan", "harga_bbm"]]
+    data_beras = datasets[["tahun", "bulan", "harga_beras"]]
     
 
-    # data_beras["prev_harga_beras"] = data_beras["harga_beras"].shift(1)
-    # data_bbm["prev_harga_bbm"] = data_bbm["harga_bbm"].shift(1)
+    data_beras["prev_harga_beras"] = data_beras["harga_beras"].shift(1)
+    data_bbm["prev_harga_bbm"] = data_bbm["harga_bbm"].shift(1)
     
-    # data_beras["diff_beras"] = (data_beras["harga_beras"] - data_beras["prev_harga_beras"])
-    # data_bbm["diff_bbm"] = (data_bbm["harga_bbm"] - data_bbm["prev_harga_bbm"])
+    data_beras["diff_beras"] = (data_beras["harga_beras"] - data_beras["prev_harga_beras"])
+    data_bbm["diff_bbm"] = (data_bbm["harga_bbm"] - data_bbm["prev_harga_bbm"])
 
-    # for inc in range(1, 13):
-    #         field_name = "lag_" + str(inc)
-    #         data_beras["beras_" + field_name] = data_beras["diff_beras"].shift(inc)
-    #         data_bbm["bbm_" + field_name] = data_bbm["diff_bbm"].shift(inc)
+    for inc in range(1, 13):
+            field_name = "lag_" + str(inc)
+            data_beras["beras_" + field_name] = data_beras["diff_beras"].shift(inc)
+            data_bbm["bbm_" + field_name] = data_bbm["diff_bbm"].shift(inc)
 
-    # model_beras = smf.ols(formula = 'diff_beras ~ beras_lag_1 + beras_lag_2 + beras_lag_3 + beras_lag_4 + beras_lag_5 + beras_lag_6 + beras_lag_7 + beras_lag_8 + beras_lag_9 + beras_lag_10 + beras_lag_11 + beras_lag_12', data = data_beras)
-    # model_bbm = smf.ols(formula = 'diff_bbm ~ bbm_lag_1 + bbm_lag_2 + bbm_lag_3 + bbm_lag_4 + bbm_lag_5 + bbm_lag_6 + bbm_lag_7 + bbm_lag_8 + bbm_lag_9 + bbm_lag_10 + bbm_lag_11 + bbm_lag_12', data = data_bbm)
+    model_beras = smf.ols(formula = 'diff_beras ~ beras_lag_1 + beras_lag_2 + beras_lag_3 + beras_lag_4 + beras_lag_5 + beras_lag_6 + beras_lag_7 + beras_lag_8 + beras_lag_9 + beras_lag_10 + beras_lag_11 + beras_lag_12', data = data_beras)
+    model_bbm = smf.ols(formula = 'diff_bbm ~ bbm_lag_1 + bbm_lag_2 + bbm_lag_3 + bbm_lag_4 + bbm_lag_5 + bbm_lag_6 + bbm_lag_7 + bbm_lag_8 + bbm_lag_9 + bbm_lag_10 + bbm_lag_11 + bbm_lag_12', data = data_bbm)
     
-    # model_beras_fit = model_beras.fit()
-    # model_bbm_fit = model_bbm.fit()
+    model_beras_fit = model_beras.fit()
+    model_bbm_fit = model_bbm.fit()
     
-    # regression_adj_rsq_beras = model_beras_fit.rsquared_adj
-    # regression_adj_rsq_bbm = model_bbm_fit.rsquared_adj
+    regression_adj_rsq_beras = model_beras_fit.rsquared_adj
+    regression_adj_rsq_bbm = model_bbm_fit.rsquared_adj
 
-    # # dataset_beras = data_beras.drop()
+    # dataset_beras = data_beras.drop()
 
-    # # train_set = data_beras[0: -6].values
-    # # test_set = data_beras[-6:].values
-    # print(data_beras)
+    # train_set = data_beras[0: -6].values
+    # test_set = data_beras[-6:].values
+    print(data_beras)
 
     
 
